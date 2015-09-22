6e581ba57168d502d900bc73a447ba8451831d1b
Parah-Salin <rempelwill@hotmail.com>
2015-09-21 23:55:09 -0600

    Lab 2
    Here is my second lab for ATLS4120

---
+++ Lab-2/RollingStones/RollingStones/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  RollingStones
+//
+//  Created by Will Rempel on 9/21/15.
+//  Copyright (c) 2015 Will Rempel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    
+    @IBAction func changeFontSize(sender: UISlider) {
+        let fontSize=sender.value
+        fontSizeLabel.text=String(format: "%.0f", fontSize)
+        let fontSizeCGFloat=CGFloat(fontSize)
+        titleLabel.font=UIFont.systemFontOfSize(fontSizeCGFloat)
+    }
+    
+    @IBOutlet weak var capitalSwitch: UISwitch!
+    
+    func updateCaps(){
+        if capitalSwitch.on {
+            titleLabel.text=titleLabel.text?.uppercaseString
+        }else{
+            titleLabel.text=titleLabel.text?.lowercaseString
+        }}
+    
+    func updateImage(){
+        if imageControl.selectedSegmentIndex==0{
+            titleLabel.text="They Used To Be Young"
+            stonesImage.image=UIImage(named : "youngStones")
+        }
+        else if imageControl.selectedSegmentIndex==1{
+            titleLabel.text="Now they are old"
+            stonesImage.image=UIImage(named: "oldStones")
+        }}
+    @IBAction func updateFont(sender: UISwitch) {
+        updateCaps()
+    }
+    @IBOutlet weak var imageControl: UISegmentedControl!
+    
+    @IBAction func changeInfo(sender: UISegmentedControl) {
+        updateImage()
+        updateCaps()
+    }
+    
+    @IBOutlet weak var stonesImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
